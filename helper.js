e3b1e918f14329aa85789086f008ff38d041607c
Ahmet Tekeli <ahmettekeli1991@hotmail.com>
2021-02-14 03:10:07 +0300

    implement the logic for scanning each document cell by cell.

--- helper.js
+++ helper.js
@@ -1,47 +1,12 @@
 const fs = require("fs");
 
 const writeOutput = (file, text) => {
-		fs.writeFile(file, text, function (err) {
-			if (err) {
-				return console.error(err);
-			}
-			console.log("Output file has been created successfully!");
-		});
-	},
-	getRange = (column, row) => {
-		let columnLetters = [
-			"A",
-			"B",
-			"C",
-			"D",
-			"E",
-			"D",
-			"F",
-			"G",
-			"H",
-			"I",
-			"J",
-			"K",
-			"L",
-			"M",
-			"N",
-			"O",
-			"P",
-			"Q",
-			"R",
-			"S",
-			"T",
-			"U",
-			"V",
-			"W",
-			"X",
-			"Y",
-			"Z",
-		];
-		return {
-			columns: columnLetters.slice(0, columnLetters.indexOf(column) + 1),
-			row,
-		};
-	};
+	fs.writeFile(file, text, function (err) {
+		if (err) {
+			return console.error(err);
+		}
+		console.log("Output file has been created successfully!");
+	});
+};
 
-module.exports = { writeOutput, getRange };
+module.exports = { writeOutput };
--- index.js
+++ index.js
@@ -1,38 +1,70 @@
 // const path = require("path");
-const xlsx = require("xlsx");
+const xlsx = require("node-xlsx");
 const helper = require("./helper");
 
-// const inputFile = "updated_zipcodes.xlsx",
-// 	compareFile = "outdated_zipcodes.xlsx",
-// 	outFile = "differences.xlsx",
-// 	outTxt = "differences.txt";
+// const workSheetsFromFile = xlsx.parse(`${__dirname}/plans.xlsx`);
+// console.log({ data0: workSheetsFromFile[0].data[0] }); //header
+// console.log("worksheet1:", workSheetsFromFile[0].name); //sheets
 
-const checkForDifferences = ({ inputFile, compareFile, outputFile, rangeColumn, rangeRow }) => {
-	const inputData = xlsx.readFile(inputFile),
-		inputSheet1 = inputData.SheetNames[0],
-		inputDataSheet = inputData.Sheets[inputSheet1],
-		compareData = xlsx.readFile(compareFile),
-		compareSheet1 = compareData.SheetNames[0],
-		compareDataSheet = compareData.Sheets[compareSheet1];
+const checkForDifferences = ({
+	inputFile,
+	inputSheets, //array
+	compareFile,
+	compareSheets, //array
+	outputFile,
+	rangeColumn,
+	rangeRow,
+}) => {
+	const inputExcelData = xlsx.parse(`${__dirname}/${inputFile}`).slice(0, inputSheets.length),
+		inputSheetHeaders = inputExcelData.map((sheet) => sheet.data[0]),
+		compareExcelData = xlsx.parse(`${__dirname}/${compareFile}`).slice(0, compareSheets.length),
+		compareSheetHeaders = compareExcelData.map((sheet) => sheet.data[0]);
+	let tempRowVal = [],
+		dataDifference = [];
 
-	const range = helper.getRange(rangeColumn, rangeRow);
-	for (let columnIndex = 0; columnIndex < range.columns.length; columnIndex++) {
-		for (let rowIndex = 2; rowIndex <= range.row; rowIndex++) {
-			if (
-				inputDataSheet[range.columns[columnIndex] + rowIndex].w !=
-				compareDataSheet[range.columns[columnIndex] + rowIndex].w
-			) {
-				console.log(
-					`${range.columns[columnIndex]}${rowIndex} cell has different values in the given excel files.`
-				);
-				console.log("input file -> " + inputDataSheet[range.columns[columnIndex] + rowIndex].w);
-				console.log("comparing file -> " + compareDataSheet[range.columns[columnIndex] + rowIndex].w);
-				console.log("--------------");
+	for (let sheetIndex = 0; sheetIndex < inputExcelData.length; sheetIndex++) {
+		// per sheet
+		console.log("sheets:", inputExcelData[sheetIndex].name);
+		for (let rowIndex = 1; rowIndex < inputExcelData[sheetIndex].data.length; rowIndex++) {
+			//per row
+			//TODO:make sure it has the last row
+			if (compareExcelData[sheetIndex].data[rowIndex][0]) {
+				compareExcelData[sheetIndex].data[rowIndex].forEach((cell, cellIndex) => {
+					if (cell && cell !== inputExcelData[sheetIndex].data[rowIndex][cellIndex]) {
+						// console.log(`-------- Diff in cell -> Row:${rowIndex} , Column:${cellIndex} --------`);
+						if (inputExcelData[sheetIndex].data[rowIndex][cellIndex]) {
+							tempRowVal.push(inputExcelData[sheetIndex].data[rowIndex][cellIndex]);
+						} else {
+							tempRowVal.push(null);
+						}
+					}
+				});
+				dataDifference.push(tempRowVal);
+				tempRowVal = [];
 			}
 		}
+		console.log(dataDifference);
 	}
-	//print to the output file here
-	// helper.writeOutput()
+
+	// const range = helper.getRange(rangeColumn, rangeRow),
+	// const dataDifference = [],
+	// 	sheetsToBuild = inputExcelData.map((sheet, index) => {
+	// 		return {
+	// 			name: sheet.name,
+	// 			data: dataDifference[index],
+	// 		};
+	// 	}),
+	// 	buffer = xlsx.build(sheetsToBuild);
 };
 
-modile.exports = { checkForDifferences };
+checkForDifferences({
+	inputFile: "plans.xlsx",
+	inputSheets: ["sheet1"], //array
+	compareFile: "plans copy.xlsx",
+	compareSheets: ["sheet1"], //array
+	outputFile: "some output.xlsx",
+	rangeColumn: "C",
+	rangeRow: 4,
+});
+
+module.exports = { checkForDifferences };
